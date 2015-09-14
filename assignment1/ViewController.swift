2c098da3235c2b0bd26a20b891bb999e0b4ff1f0
artLopez <arlopez@csumb.edu>
2015-09-13 18:39:42 -0700

    Initial Commit

---
+++ assignment1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  assignment1
+//
+//  Created by Arturo Lopez on 9/13/15.
+//  Copyright (c) 2015 Arturo Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
