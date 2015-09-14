bbc237bd7944360b83bc28096eaeb6205878ee12
artLopez <arlopez@csumb.edu>
2015-09-14 01:33:08 -0700

    Finished creating button layout for calculator and now going to refactor the switch case statement

--- assignment1/ViewController.swift
+++ assignment1/ViewController.swift
@@ -11,7 +11,58 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var display: UILabel!
- 
+    
+    var userMiddleTypingNum = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userMiddleTypingNum {
+            display.text = display.text! + digit
+        }else{
+            display.text = digit
+            userMiddleTypingNum = true
+        }
 
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userMiddleTypingNum{
+            enter()
+        }
+        switch operation {
+            case "×":
+                if operandStack.count >= 2 {
+                    displayValue = operandStack.removeLast() * operandStack.removeLast()
+                    enter()
+                }
+                
+//            case "÷":
+//            case "+":
+//            case "−":
+            default: break
+        }
+        
+        
+    }
+    
+    var operandStack = Array<Double>()
+
+    
+    @IBAction func enter() {
+        userMiddleTypingNum = false
+        operandStack.append(displayValue)
+        println("operaandStack = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+            userMiddleTypingNum = false
+        }
+    }
 }
 
