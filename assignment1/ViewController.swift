2b89f61b66e50c9fb3ed521000da5c366ec210d4
artLopez <arlopez@csumb.edu>
2015-09-15 22:38:34 -0700

    Fixed autolayout and adding functionality

--- assignment1/ViewController.swift
+++ assignment1/ViewController.swift
@@ -11,53 +11,105 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var inputHistory: UILabel!
+    
     
     var userMiddleTypingNum = false
+    var hasDecimal = false
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
-        if userMiddleTypingNum {
-            display.text = display.text! + digit
+    
+        if userMiddleTypingNum{
+            if( digit == "." && display.text!.rangeOfString(".") == nil || digit != "."){
+                
+                inputHistory.text = inputHistory.text! + digit
+            }
+        }
+        else {
+            checkIfPi(digit)
+            inputHistory.text = inputHistory.text! + " " + digit
+            userMiddleTypingNum = true
+        }
+    }
+    
+    @IBAction func clearButton(sender: UIButton) {
+        operandStack.removeAll()
+        display.text = "0"
+        userMiddleTypingNum = false
+        inputHistory.text = ""
+    }
+    @IBAction func operate(sender: UIButton) {
+        if checkDoubleOperation(sender.currentTitle!) && operandStack.count > 1{
+            inputHistory.text = inputHistory.text! + " " + sender.currentTitle!
+        }
+        
+        if userMiddleTypingNum{
+            enter()
+        }
+        if let operation = sender.currentTitle{
+            switch operation {
+            case "×": performOperation{ $0 * $1}
+            case "÷": performOperation{ $1 / $0}
+            case "+": performOperation{ $0 + $1}
+            case "−": performOperation{ $1 - $0}
+            case "√" : performOperation { sqrt($0)}
+            case "sin": performOperation{ sin($0) }
+            case "cos": performOperation{ cos($0) }
+                
+            default: break
+            }
+        }
+    }
+    
+    func checkIfPi(digit: String){
+        if(digit ==  "π"){
+            display.text = "3.14159"
         }else{
             display.text = digit
-            userMiddleTypingNum = true
         }
 
     }
     
-    @IBAction func operate(sender: UIButton) {
-        let operation = sender.currentTitle!
-        if userMiddleTypingNum{
+    func checkDoubleOperation(operation: String) -> Bool{
+        if operation == "×" || operation == "+" ||
+                        operation == "−" || operation == "÷"{
+            return true
+        }
+        return false
+    }
+    
+    
+    func performOperation(operation: (Double,Double) -> Double){
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
             enter()
         }
-        switch operation {
-            case "×":
-                if operandStack.count >= 2 {
-                    displayValue = operandStack.removeLast() * operandStack.removeLast()
-                    enter()
-                }
-                
-//            case "÷":
-//            case "+":
-//            case "−":
-            default: break
+    }
+  
+    private func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
         }
-        
-        
     }
     
     var operandStack = Array<Double>()
 
-    
     @IBAction func enter() {
         userMiddleTypingNum = false
         operandStack.append(displayValue)
-        println("operaandStack = \(operandStack)")
+        println("operandStack = \(operandStack)")
     }
     
     var displayValue: Double {
         get{
-            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+            if(display.text! == "π"){
+                return M_PI
+            }
+            else{
+                return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+            }
         }
         set{
             display.text = "\(newValue)"
